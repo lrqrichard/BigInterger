49c28774c25ad83a3c051900c465941549a10c8f
Renqian Luo (MSR Student-Person Consulting) <v-reluo@microsoft.com>
2015-10-25 13:33:07 +0800

    rewrite main in C++ to use string to implement input

--- Big Interger/main.cpp
+++ Big Interger/main.cpp
@@ -28,28 +28,27 @@ int _tmain(int argc, _TCHAR* argv[])
 			num3 = (char*)malloc(sizeof(char)*(maxlen + 2));
 			BigAdd(num1, num2, num3);
 			cout<<num3<<endl;
-			free(num3);
 		}
 		else if (op == '-'){
 			num3 = (char*)malloc(sizeof(char)*(maxlen + 2));
 			BigSub(num1, num2, num3);
 			cout<<num3<<endl;
-			free(num3);
 		}
 		else if (op == '*'){
 			num3 = (char*)malloc(sizeof(char)*(len1 + len2 + 1));
 			BigMul(num1, num2, num3);
 			cout<<num3<<endl;
-			free(num3);
 		}
 		else if (op == '/'){
 			num3 = (char*)malloc(sizeof(char)*(len1 + 2));
 			num4 = (char*)malloc(sizeof(char)*(len1 + 2));
 			BigDiv(num1, num2, num3, num4);
 			cout<<num3<<endl<<num4<<endl;
-			free(num3);
 			free(num4);
 		}	
+		free(num1);
+		free(num2);
+		free(num3);
 	}
 	return 0;
 }
