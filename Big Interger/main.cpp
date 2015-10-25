a234bb4cacf1f2397d4e7dafc731515f7efa09bc
Renqian Luo (MSR Student-Person Consulting) <v-reluo@microsoft.com>
2015-10-25 19:50:14 +0800

    use x64

--- Big Interger/main.cpp
+++ Big Interger/main.cpp
@@ -14,41 +14,40 @@ int _tmain(int argc, _TCHAR* argv[])
 	char *num1, *num2, *num3, *num4;
 	string str1, str2;
 	int len1, len2, maxlen;
-	while (cin>>op){
-		cin >> str1;
-		len1 = str1.size();
-		num1 = (char*)malloc(sizeof(char)*(len1 + 1));
-		strcpy(num1, str1.c_str());
-		cin >> str2;
-		len2 = str2.size();
-		num2 = (char*)malloc(sizeof(char)*(len2 + 1));
-		strcpy(num2, str2.c_str());
-		maxlen = len1 > len2 ? len1 : len2;
-		if (op == '+'){
-			num3 = (char*)malloc(sizeof(char)*(maxlen + 2));
-			BigAdd(num1, num2, num3);
-			cout<<num3<<endl;
-		}
-		else if (op == '-'){
-			num3 = (char*)malloc(sizeof(char)*(maxlen + 2));
-			BigSub(num1, num2, num3);
-			cout<<num3<<endl;
-		}
-		else if (op == '*'){
-			num3 = (char*)malloc(sizeof(char)*(len1 + len2 + 1));
-			BigMul(num1, num2, num3);
-			cout<<num3<<endl;
-		}
-		else if (op == '/'){
-			num3 = (char*)malloc(sizeof(char)*(len1 + 2));
-			num4 = (char*)malloc(sizeof(char)*(len1 + 2));
-			BigDiv(num1, num2, num3, num4);
-			cout<<num3<<endl<<num4<<endl;
-			free(num4);
-		}	
-		free(num1);
-		free(num2);
-		free(num3);
+	cin >> op;
+	cin >> str1;
+	len1 = str1.size();
+	num1 = (char*)malloc(sizeof(char)*(len1 + 1));
+	strcpy(num1, str1.c_str());
+	cin >> str2;
+	len2 = str2.size();
+	num2 = (char*)malloc(sizeof(char)*(len2 + 1));
+	strcpy(num2, str2.c_str());
+	maxlen = len1 > len2 ? len1 : len2;
+	if (op == '+'){
+		num3 = (char*)malloc(sizeof(char)*(maxlen + 2));
+		BigAdd(num1, num2, num3);
+		cout<<num3<<endl;
 	}
+	else if (op == '-'){
+		num3 = (char*)malloc(sizeof(char)*(maxlen + 2));
+		BigSub(num1, num2, num3);
+		cout<<num3<<endl;
+	}
+	else if (op == '*'){
+		num3 = (char*)malloc(sizeof(char)*(len1 + len2 + 1));
+		BigMul(num1, num2, num3);
+		cout<<num3<<endl;
+	}
+	else if (op == '/'){
+		num3 = (char*)malloc(sizeof(char)*(len1 + 2));
+		num4 = (char*)malloc(sizeof(char)*(len1 + 2));
+		BigDiv(num1, num2, num3, num4);
+		cout<<num3<<endl<<num4<<endl;
+		free(num4);
+	}	
+	free(num1);
+	free(num2);
+	free(num3);
 	return 0;
 }
